30989d89ac79a19d86e9722dfafe8e717e0562eb
Sang - Ik Lee <root@npip.iptime.org>
2018-10-09 02:27:46 +0900

    add_licence-page

---
+++ js/config.js
@@ -0,0 +1,15 @@
+function main_page_url(){
+    window.open('https://github.com/devstar1224','newWindow');
+}
+
+function git_main_url(){
+  window.open('https://github.com/devstar1224','newWindow');
+}
+
+function git_bot_url(){
+    window.open('https://github.com/devstar1224/Popcon_discord','newWindow');
+}
+
+function bot_invite_url(){
+    window.open('https://discordapp.com/oauth2/authorize?client_id=474229020012052484&permissions=0&scope=bot','newWindow');
+}
